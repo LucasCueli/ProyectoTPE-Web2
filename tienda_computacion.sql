7f633ff0e21d839eead775978c74a72bbea677b3
Lucas Cueli <lucascueli4@gmail.com>
2023-10-11 16:46:43 -0300

    Tabla usuarios creada

--- tienda_computacion.sql
+++ tienda_computacion.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-09-2023 a las 23:57:23
+-- Tiempo de generación: 11-10-2023 a las 21:46:01
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -110,6 +110,26 @@ CREATE TABLE `rams` (
   `Valor` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `username` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `email`, `password`, `username`) VALUES
+(1, 'webadmin', '$2y$10$NLFFfBx0gkF3nuNLD/B1BuaTkVJ8wIr..WVphVctrv1f2R16dNSiu', '');
+
 --
 -- Índices para tablas volcadas
 --
@@ -147,6 +167,22 @@ ALTER TABLE `procesadores`
 --
 ALTER TABLE `rams`
   ADD PRIMARY KEY (`ID_RAM`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
