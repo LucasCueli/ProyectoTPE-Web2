f4d6b45ca6acab8c3ecc62790863294322b3ff9d
Lucas Cueli <lucascueli4@gmail.com>
2023-10-16 16:38:01 -0300

    Boton de 'Añadir al carrito' agregado

--- views/page.view.php
+++ views/page.view.php
@@ -37,7 +37,7 @@
             echo "<section class='productos'>";
             $procesadores = $this->model->getAllProcesadores();
             foreach($procesadores as $proce){
-                echo '<article class="producto">' . $proce->Marca . '<h2>' . $proce->Modelo . '</h2>' .  '<p>' . $proce->Socket . '<br>' . $proce->Valor . '</p>' .'</article>';
+                echo '<article class="producto">' . '<h2>' . $proce->Marca . '<h2>' . '<h2>' . $proce->Modelo . '</h2>' .  '<h3>' . $proce->Socket . '<br>' . '<br>' . '$' . $proce->Valor . '</h3>' . '<div class="button_slide slide_left"><a href="#">Añadir al carrito</a></div>' .'</article>';
             }
             echo '</section>';
             include_once "./templates/footer.tpl" ; 
@@ -48,7 +48,7 @@
             echo "<section class='productos'>";
             $placas = $this->model->getAllGraficas();
             foreach($placas as $placa){
-                echo '<article class="producto">' . $placa->Marca . '<h2>' . $placa->Modelo . '</h2>' .  '<p>' . $placa->Vram . '<br>' . $placa->Valor . '</p>' .'</article>';
+                echo '<article class="producto">' . '<h2>' . $placa->Marca . '<h2>' . '<h2>' . $placa->Modelo . '</h2>' .  '<h3>' . $placa->Vram . '<br>' . '<br>' . '$' . $placa->Valor . '</h3>' . '<div class="button_slide slide_left"><a href="#">Añadir al carrito</a></div>' .'</article>';
             }
             echo '</section>';
             include_once "./templates/footer.tpl" ; 
@@ -59,7 +59,7 @@
             echo "<section class='productos'>";
             $rams = $this->model->getAllRams();
             foreach($rams as $ram){
-                echo '<article class="producto">' . $ram->Marca . '<h2>' . $ram->Tamaño . '</h2>' .  '<p>' . $ram->Generacion . '<br>' . $ram->Velocidad . '<br>' . $ram->Valor . '</p>' . '</article>';
+                echo '<article class="producto">' . '<h2>' . $ram->Marca . '<h2>' . '<h2>' . $ram->Tamaño . '</h2>' .  '<h3>' . $ram->Generacion . '<br>' . $ram->Velocidad . '<br>' . '<br>' . '$' . $ram->Valor . '</h3>' . '<div class="button_slide slide_left"><a href="#">Añadir al carrito</a></div>' . '</article>';
             }
             echo '</section>';
             include_once "./templates/footer.tpl" ; 
@@ -70,7 +70,7 @@
             echo "<section class='productos'>";
             $gabinetes = $this->model->getAllGabinetes();
             foreach($gabinetes as $gabinete){
-                echo '<article class="producto">' . $gabinete->Marca . '<h2>' . $gabinete->Modelo . '</h2>' .  '<p>' . $gabinete->Tamaño . '<br>' . $gabinete->Valor . '</p>' .'</article>';
+                echo '<article class="producto">' . '<h2>' . $gabinete->Marca . '<h2>' . '<h2>' . $gabinete->Modelo . '</h2>' .  '<h3>' . $gabinete->Tamaño . '<br>' . '<br>' . '$' . $gabinete->Valor . '</h3>' . '<div class="button_slide slide_left"><a href="#">Añadir al carrito</a></div>' .'</article>';
             }
             echo '</section>';
             include_once "./templates/footer.tpl" ; 
